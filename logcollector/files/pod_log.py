74768670eb8d62294ea7f89ebd1d09985649ccfb
aiopsdata <91112244+aiopsdata@users.noreply.github.com>
2021-09-30 00:21:57 +0530

    edited to fetch latest logs

--- logcollector/files/pod_log.py
+++ logcollector/files/pod_log.py
@@ -19,8 +19,8 @@ except config.ConfigException as e:
 
 pod_list_api = client.CoreV1Api()                            # pod_list_api Object
 document_id = 0
-
-while(1):
+running_time = 0
+while(True):
     pod_list = pod_list_api.list_pod_for_all_namespaces()        # List all pods from all namespaces 
 
     
@@ -45,18 +45,24 @@ while(1):
         if pod.status.container_statuses[0].ready is False:                 # Check if pod is not ready
             continue
             
-         c_name=pod.spec.containers[0].name
+        c_name=pod.spec.containers[0].name
+        if running_time == 0:
+            data = pod_list_api.read_namespaced_pod_log(namespace = namespace,name = podname, container = c_name)
+        else:
+            data = pod_list_api.read_namespaced_pod_log(namespace = namespace,name = podname, container = c_name, since_seconds=1)
+        if data != "":
+            date = datetime.now()
+            date = date.strftime("%d/%m/%Y %H:%M:%S")
+            data_dic = json.dumps(dict({'agent' : "log-collector",'datetime': date,'namespace' : namespace, 'pod': podname , 'time': round(time.time(), 2), 'podlog': data}))
 
-        data = pod_list_api.read_namespaced_pod_log(namespace = namespace,name = podname, container = c_name)
-        date = datetime.now()
-        date = date.strftime("%d/%m/%Y %H:%M:%S")
-        data_dic = json.dumps(dict({'agent' : "log-collector",'datetime': date,'namespace' : namespace, 'pod': podname , 'time': round(time.time(), 2), 'podlog': data}))
-
-        try:
-            es.index(index=os.environ['PINDEX_NAME'],doc_type=os.environ['PDOC_TYPE'],id=document_id,body=data_dic,ignore=400)
-        except ElasticsearchException as e:
-            print("\n Exception when calling ElasticsearchApi->creating connection: %s\n" % e)
+            try:
+                es.index(index=os.environ['PINDEX_NAME'],doc_type=os.environ['PDOC_TYPE'],id=document_id,body=data_dic,ignore=400)
+            except ElasticsearchException as e:
+                print("\n Exception when calling ElasticsearchApi->creating connection: %s\n" % e)
 
         document_id =  document_id + 1
+    time.sleep(1)
+    running_time = 1
+      
 
 
